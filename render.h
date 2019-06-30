d13640474621835b00d22afeb94df45dbd091768
Pooyan Khanjankhani <pooyankhan@gmail.com>
2019-06-30 14:04:24 +0430

    fixed render.h include objects

--- render.h
+++ render.h
@@ -1,14 +1,11 @@
 #define WIDTH 40
 #define HEIGHT 40
 
-// #include "object.h"
+#include "objects.h"
 
 typedef struct Cell Cell;
 typedef struct Point Point;
 
-struct Point{
-    int x,y;
-};
 struct Cell
 {
     Point point;

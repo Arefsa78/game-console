7e5ef4cd2175e10ff94a9fa19327c38c9734792c
Pooyan Khanjankhani <pooyankhan@gmail.com>
2019-06-30 15:27:17 +0430

    working gui with main funcition and not-working one as comment

--- render.h
+++ render.h
@@ -1,13 +1,33 @@
-#define WIDTH 40
-#define HEIGHT 40
 
 #include "objects.h"
 
 typedef struct Cell Cell;
-typedef struct Point Point;
+typedef struct GUI_Settings GUI_Settings;
 
 struct Cell
 {
     Point point;
     char color;
+};
+
+struct GUI_Settings{
+    int height ,width;
+    // Colors
+
+
+    int solid_block;//
+    int death_block;//
+    int move_block;//
+    int wall;//
+
+    int charecter;//
+    int target;//
+    int object;//
+
+    int attack;//
+
+    int rpoints;//
+    int opp;//
+    int put;//
+
 };
